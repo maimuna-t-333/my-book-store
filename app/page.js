9f4506f8ae74e48f76cfe6c5c6196e97ab688e33
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-09-04 00:24:26 +0600

    final finishing

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import About from "./Components/About.js"
 import Categories from "./Components/Categories.js"
+import Footer from "./Components/Footer.js"
 import Hero2 from "./Components/Hero.js"
 import Navbar from "./Components/Navbar.js"
 import Newsletter from "./Components/Newsletter.js"
+import PublishersMarquee from "./Components/PublishersMarquee.js"
 
 export default function Home() {
   return (
@@ -10,10 +12,14 @@ export default function Home() {
       <Navbar />
       {/* Other sections here */}
       <Hero2></Hero2>
+     
       
       <Categories></Categories>
+       <PublishersMarquee></PublishersMarquee>
       <About></About>
+      
       <Newsletter></Newsletter>
+      <Footer></Footer>
     </main>
   )
 }
